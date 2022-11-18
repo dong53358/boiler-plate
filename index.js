9cacc61565e7d85703283cd9fbfedc3e6992258c
dong53358 <dong53358@naver.com>
2022-11-18 23:56:36 +0900

    로그아웃 기능 구현

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
-
+const { auth } = require("./middleware/auth.js");
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -27,7 +27,7 @@ app.get("/", (req, res) => {
   res.send("Hello World! 취뽀..");
 });
 
-app.post("/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   // 회원 강비 할때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
 
@@ -40,7 +40,7 @@ app.post("/register", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
     if (!user) {
@@ -49,27 +49,47 @@ app.post("/login", (req, res) => {
         message: "제공된 이메일에 해당하는 유저가 없습니다.",
       });
     }
-  });
+    // 요청된 이메일이 데이터 베이스에 있따면 비밀번호가 맞는 비밀번호 인지 확인.
 
-  // 요청된 이메일이 데이터 베이스에 있따면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      // 비밀번호 까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
 
-  user.comparePassword(req.body.password, (err, isMatch) => {
-    if (!isMatch)
-      return res.json({
-        loginSuccess: false,
-        message: "비밀번호가 틀렸습니다.",
+        // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
       });
+    });
   });
+});
 
-  // 비밀번호 까지 맞다면 토큰을 생성하기.
-  user.generateToken((err, user) => {
-    if (err) return res.status(400).send(err);
+// role 0 -> 일반유저, role 0이 아니면 관리자
+app.get("/api/users/auth", auth, (req, res) => {
+  // 여기 까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True 라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
 
-    // 토큰을 저장한다. 어디에 ? 쿠키, 로컬스토리지
-    res
-      .cookie("x_auth", user.token)
-      .status(200)
-      .json({ loginSuccess: true, userId: user._id });
+app.get("/api/users/logout", auth, (res, req) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({ success: true });
   });
 });
 
--- models/User.js
+++ models/User.js
@@ -61,12 +61,27 @@ userSchema.methods.comparePassword = function (plainPassword, cb) {
 userSchema.methods.generateToken = function (cb) {
   // jsonwebtoken을 이용해서 token을 생성하기
   var user = this;
-  var token = jwt.sign(user._id, "secretToken");
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
 
   user.token = token;
   user.save(function (err, user) {
     if (err) return cb(err);
     cb(null, user);
+  });
+};
+
+userSchema.statics.findByToken = function (token, cb) {
+  var user = this;
+
+  // 토큰을 decode 한다.
+  jwt.verify(token, "secretToken", function (err, decoded) {
+    // 유저 아이디를 이용해서 유저를 찾은 다음에
+    // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
+
+    User.findOne({ _id: decoded, token: token }, function (err, user) {
+      if (err) return cb(err);
+      cb(null, user);
+    });
   });
 };
 
