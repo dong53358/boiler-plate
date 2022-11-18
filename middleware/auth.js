b07f242a0ca029e902e363cfe8294af63415cdd6
dong53358 <dong53358@naver.com>
2022-11-19 00:49:10 +0900

    로그아웃 토큰제거 에러해결

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,7 +4,7 @@ let auth = (req, res, next) => {
   // 인증처리를 하는 곳
 
   // 클라이언트 쿠키에서 토큰을 가져온다.
-  let token = req.cokkies.x_auth;
+  let token = req.cookies.x_auth;
 
   // 토큰을 복호화 한후 유저를 찾는다.
   User.findByToken(token, (err, user) => {
@@ -21,4 +21,4 @@ let auth = (req, res, next) => {
   // 유저가 없으면 인증 no
 };
 
-module.exports - { auth };
+module.exports = { auth };
