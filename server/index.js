2879ead7be121cc42ca5e0f269f6ed61f3e9fc59
dong53358 <dong53358@naver.com>
2022-11-29 23:36:38 +0900

    cors 이슈, proxy 설정

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
@@ -25,6 +25,10 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World! 취뽀..");
+});
+
+app.get("/api/hello", (req, res) => {
+  res.send("안녕하세요 ~");
 });
 
 app.post("/api/users/register", (req, res) => {
@@ -94,6 +98,8 @@ app.get("/api/users/logout", auth, (req, res) => {
     });
   });
 });
+
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
