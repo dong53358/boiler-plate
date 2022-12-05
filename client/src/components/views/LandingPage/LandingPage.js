be6aef43bb47c8ff94c52073498dd572d0cebed4
dong53358 <dong53358@naver.com>
2022-12-05 21:25:01 +0900

    회원가입, 로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 const Main = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
@@ -13,11 +15,27 @@ const Main = styled.div`
 `;
 
 function LandingPage() {
+  const navigate = useNavigate();
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
 
-  return <Main>시작 페이지</Main>;
+  const onClickHandle = () => {
+    axios.get("/api/users/logout").then((response) => {
+      console.log(response.data);
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃 실패");
+      }
+    });
+  };
+  return (
+    <Main>
+      <h1>시작페이지</h1>
+      <button onClick={onClickHandle}>로그아웃</button>
+    </Main>
+  );
 }
 
 export default LandingPage;
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import loginUser, { registerUser } from "../../../_actions/user_action";
+import { useNavigate } from "react-router-dom";
 
 const Main = styled.div`
   display: flex;
@@ -12,8 +15,87 @@ const Main = styled.div`
   font-weight: bold;
 `;
 
+const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  font-size: 20px;
+  font-weight: 400;
+`;
+
 function RegisterPage() {
-  return <Main>RegisterPage</Main>;
+  const [email, setEmailDate] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      return alert("비밀번호와 비밀번호 확인은 같아야 합니다.");
+    }
+
+    let body = {
+      email: email,
+      password: password,
+      name: name,
+    };
+
+    dispatch(registerUser(body)).then((response) => {
+      if (response.payload.success) {
+        navigate("/login");
+      } else {
+        alert("회원가입 실패");
+      }
+    });
+
+    setEmailDate("");
+    setName("");
+    setPassword("");
+    setConfirmPassword("");
+  };
+
+  const handleChange = (event) => {
+    if (event.target.type === "email") {
+      setEmailDate(event.target.value);
+    } else if (event.target.type === "text") {
+      setName(event.target.value);
+    } else if (event.target.type === "password") {
+      setPassword(event.target.value);
+    }
+  };
+  const comfirmhandle = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  return (
+    <Main>
+      <LoginForm onSubmit={onSubmit}>
+        <label>Email</label>
+        <input type="email" value={email} onChange={handleChange}></input>
+
+        <label>Name</label>
+        <input type="text" value={name} onChange={handleChange}></input>
+
+        <label>Password</label>
+        <input type="password" value={password} onChange={handleChange}></input>
+
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={comfirmhandle}
+        ></input>
+
+        <br />
+
+        <button type="submit">회원가입</button>
+      </LoginForm>
+    </Main>
+  );
 }
 
 export default RegisterPage;
